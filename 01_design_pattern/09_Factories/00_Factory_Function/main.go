d69d4de37ce88d6c65939e28e7c70a2989982744
Hikarivina <dangnguyenhuu88@gmail.com>
2020-05-01 11:59:31 +0900

    DP: Factory Function

---
+++ 01_design_pattern/09_Factories/00_Factory_Function/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Name     string
+	Age      int
+	EyeCount int
+}
+
+func NewPerson(name string, age int) *Person {
+
+	if age < 16 {
+
+	}
+	return &Person{name, age, 2}
+
+}
+
+func main() {
+
+	b := NewPerson("Johny", 2)
+
+	fmt.Println(b)
+
+}
